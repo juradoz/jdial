74a692f8340919a37d918ace7e6e9731cba100db
Daniel Jurado <jurado@gmail.com>
2014-07-10 22:14:45 -0300

    Injecting by constructor

--- jdial-cti/src/main/java/al/jdi/cti/util/Main.java
+++ jdial-cti/src/main/java/al/jdi/cti/util/Main.java
@@ -1,24 +1,33 @@
 package al.jdi.cti.util;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.joda.time.Period;
 
 import al.jdi.cti.CtiManager;
 import al.jdi.cti.CtiManager.CtiManagerService;
 
 public class Main {
 
-	@CtiManagerService
-	@Inject
-	private CtiManager ctiManager;
+	private final CtiManager ctiManager;
 
-	public void teste(@Observes ContainerInitialized event)
-			throws InterruptedException {
+	@Inject
+	public Main(@CtiManagerService CtiManager ctiManager) {
+		this.ctiManager = ctiManager;
+	}
+
+	@PostConstruct
+	public void start() {
 		ctiManager.start();
-		Thread.sleep(10000);
+	}
+
+	public void main(@Observes ContainerInitialized event)
+			throws InterruptedException {
+		Thread.sleep(Period.seconds(10).toStandardSeconds().getSeconds() * 1000);
 	}
 
 	@PreDestroy
