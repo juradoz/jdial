44cf1d385640a49197c1c3c3ae18c0b4fb68160b
Daniel Jurado <jurado@gmail.com>
2014-07-16 21:30:15 -0300

    Trying to add tiles

---
+++ jdial-all/src/main/java/al/jdi/web/controller/TilesPathResolver.java
@@ -0,0 +1,26 @@
+package al.jdi.web.controller;
+
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.http.FormatResolver;
+import br.com.caelum.vraptor.view.DefaultPathResolver;
+
+@Specializes
+public class TilesPathResolver extends DefaultPathResolver {
+
+    @Inject
+    protected TilesPathResolver(FormatResolver resolver) {
+        super(resolver);
+    }
+
+    @Override
+    protected String getPrefix() {
+        return "/";
+    }
+
+    @Override
+    protected String getExtension() {
+        return "tiles";
+    }
+}
