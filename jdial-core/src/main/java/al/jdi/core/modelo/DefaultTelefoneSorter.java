6722d4e2015de8c191344f63c2ca9d304dcc010c
Daniel Jurado <jurado@gmail.com>
2014-08-05 20:55:19 -0300

    Sorter with priorizaMailing

--- jdial-core/src/main/java/al/jdi/core/modelo/DefaultTelefoneSorter.java
+++ jdial-core/src/main/java/al/jdi/core/modelo/DefaultTelefoneSorter.java
@@ -1,10 +1,13 @@
 package al.jdi.core.modelo;
 
+import static ch.lambdaj.Lambda.filter;
+import static ch.lambdaj.Lambda.having;
 import static ch.lambdaj.Lambda.on;
 import static org.apache.commons.collections.ComparatorUtils.chainedComparator;
+import static org.hamcrest.CoreMatchers.is;
 
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import al.jdi.core.tenant.Tenant;
@@ -15,16 +18,30 @@ import ch.lambdaj.function.compare.ArgumentComparator;
 class DefaultTelefoneSorter implements TelefoneSorter {
 
   @Override
-  @SuppressWarnings({"rawtypes", "unchecked"})
   public List<Telefone> sort(Tenant tenant, List<Telefone> telefones) {
-    if (tenant.getConfiguracoes().isPriorizaCelular()) {
-      Collections.sort(telefones);
-      return telefones;
-    }
+    if (tenant.getConfiguracoes().isPriorizaCelular())
+      return celularSort(telefones);
 
+    return defaultSort(telefones);
+  }
+
+  private List<Telefone> celularSort(List<Telefone> telefones) {
+    List<Telefone> celulares =
+        defaultSort(filter(having(on(Telefone.class).isCelular(), is(true)), telefones));
+    List<Telefone> fixos =
+        defaultSort(filter(having(on(Telefone.class).isCelular(), is(false)), telefones));
+    LinkedList<Telefone> result = new LinkedList<Telefone>();
+    result.addAll(celulares);
+    result.addAll(fixos);
+    return result;
+  }
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private List<Telefone> defaultSort(List<Telefone> telefones) {
     ArgumentComparator byPrioridade = new ArgumentComparator(on(Telefone.class).getPrioridade());
     ArgumentComparator byId = new ArgumentComparator(on(Telefone.class).getId());
     Comparator orderBy = chainedComparator(byPrioridade, byId);
     return Lambda.sort(telefones, on(Telefone.class), orderBy);
   }
+
 }
